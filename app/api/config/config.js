c1deca40acb19f8f42280a8d275ce33b5cc17149
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2019-04-07 19:53:37 +0200

    Removed legacy files

--- app/api/config/config.js
+++
@@ -1,8 +0,0 @@
-var environment = process.env.NODE_ENV || 'development';
-
-var config = require('./env/' + environment);
-
-config.someEnvAgnosticSetting = true;
-
-// export config
-module.exports = config;
