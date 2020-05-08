912af91bebb7722132ae29fbb161dd216bdfa6bf
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2020-05-08 17:36:41 +0200

    functional end-to-end test update

--- tests/scoring/one-on-one.js
+++ tests/scoring/one-on-one.js
@@ -7,11 +7,15 @@
 
 const { Client } = require('pg')
 const axios = require('axios')
-// const async = require('async')
 const config = require('../../app/ingestion/config/config')
 const util = require('util')
 
-const testOrganisationId = 'c2dc095a-40f5-41d1-ad7b-55a552018a89'
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
 
 const scoreOneOnOne = async () => {
 
@@ -25,16 +29,15 @@ const scoreOneOnOne = async () => {
                                             LIMIT 10`)
 
     for(const index in trajectories.rows) {
+      console.log(`trajectory ${index}`)
       let TP = 0 
       let FP = 0 
       let FN = 0 
 
       const trajectory = trajectories.rows[index]
+      const userId = uuidv4()
       
       const points = trajectory.linestring.substr(trajectory.linestring.lastIndexOf('(') + 1, trajectory.linestring.lastIndexOf(')')).split(',')
-      
-      const response = await axios.post('http://localhost:8001/api/v1/device/create', { organisationId: testOrganisationId })
-      const token = response.data.token
       
       for(const index in points) {
         const point = points[index]
@@ -45,10 +48,11 @@ const scoreOneOnOne = async () => {
           data: {
             lat: pointDetails[1], 
             lon: pointDetails[0],
-            datetime: pointDetails[2] 
+            datetime: pointDetails[2], 
+            userId: userId
           },
-          url: 'http://localhost:8001/api/v1/device/score', 
-          headers: { 'Authorization': `Bearer ${token}`}
+          url: 'http://localhost:8001/api/v1/device/score',
+          headers: { 'X-Transaction-ID': uuidv4() }
         })
         console.log('pointResponse: ', util.inspect(pointResponse.data, false, null, true))
 
