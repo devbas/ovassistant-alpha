e1c14d310c4a4ce7afea127886ad998814c11734
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2020-05-22 20:46:03 +0200

    Debugging

--- app/ingestion/server.js
+++ app/ingestion/server.js
@@ -132,30 +132,30 @@ sock.on('message', async (topic, message) => {
           }
         })
       } else if(_.has(result, 'PutReisInformatieBoodschapIn.ReisInformatieProductDVS.0.DynamischeVertrekStaat.0')) {
-        // result = _.get(result, 'PutReisInformatieBoodschapIn.ReisInformatieProductDVS.0.DynamischeVertrekStaat.0')
-        // let id = _.get(result, 'RitId.0')
+        result = _.get(result, 'PutReisInformatieBoodschapIn.ReisInformatieProductDVS.0.DynamischeVertrekStaat.0')
+        let id = _.get(result, 'RitId.0')
         
-        // result.delay_seconds = 0 
-        // if (_.has(result, 'Trein.0.ExacteVertrekVertraging.0') && _.get(result, 'Trein.0.ExacteVertrekVertraging') !== 'PT0S') {
-        //   result.has_delay = true;
-        //   result.delay_seconds = utils.durationToSeconds(_.get(result, 'Trein.0.ExacteVertrekVertraging.0'))
-        // }
+        result.delay_seconds = 0 
+        if (_.has(result, 'Trein.0.ExacteVertrekVertraging.0') && _.get(result, 'Trein.0.ExacteVertrekVertraging') !== 'PT0S') {
+          result.has_delay = true;
+          result.delay_seconds = utils.durationToSeconds(_.get(result, 'Trein.0.ExacteVertrekVertraging.0'))
+        }
 
-        // if (result.delay_seconds < 60) {
-        //   result.has_delay = false
-        //   // delete result.delay_seconds;
-        // } 
+        if (result.delay_seconds < 60) {
+          result.has_delay = false
+          // delete result.delay_seconds;
+        } 
 
-        // if(id) {
-        //   id = 'train:' + id; 
-        //   // redisImportController.locationSanitaryCheck(id, redisWrapper)
-        //   result.type = 'train'
-        //   result.updated = Math.round((new Date()).getTime() / 1000)
-        //   result.destination = _.get(result, 'Trein.0.PresentatieTreinEindBestemming.0.Uitingen.0.Uiting.0')
-        //   result.subType = result.Trein[0].TreinSoort[0]._
-        //   result.measurementTimestamp = new Date().toJSON()
-        //   redisImportController.updateData(id, result, pgPool)
-        // }
+        if(id) {
+          id = 'train:' + id; 
+          // redisImportController.locationSanitaryCheck(id, redisWrapper)
+          result.type = 'train'
+          result.updated = Math.round((new Date()).getTime() / 1000)
+          result.destination = _.get(result, 'Trein.0.PresentatieTreinEindBestemming.0.Uitingen.0.Uiting.0')
+          result.subType = result.Trein[0].TreinSoort[0]._
+          result.measurementTimestamp = new Date().toJSON()
+          // redisImportController.updateData(id, result, pgPool)
+        }
       }
     })
   } catch(err) {
@@ -194,42 +194,42 @@ sock1.on('message', async (topic, message) => {
                 // redisClient.zrem('items', id)
               } else {
                 // console.log('positionMessage: ', positionMessage)
-                // const nowUnix = Math.round((new Date()).getTime() / 1000)
+                const nowUnix = Math.round((new Date()).getTime() / 1000)
 
-                // const data = {
-                //   id: id, 
-                //   type: 'vehicle', 
-                //   agencyCode: positionMessage.dataownercode[0], 
-                //   datetimeUnix: nowUnix, 
-                //   measurementTimestamp: positionMessage.timestamp[0]
-                // }
+                const data = {
+                  id: id, 
+                  type: 'vehicle', 
+                  agencyCode: positionMessage.dataownercode[0], 
+                  datetimeUnix: nowUnix, 
+                  measurementTimestamp: positionMessage.timestamp[0]
+                }
 
-                // if (positionMessage['rd-x'] && positionMessage['rd-x'][0] != -1 && positionMessage['rd-y'][0] != -1) {
-                //   data.latitude = utils.RD2lat(positionMessage['rd-x'][0], positionMessage['rd-y'][0]);
-                //   data.longitude = utils.RD2lng(positionMessage['rd-x'][0], positionMessage['rd-y'][0]);
+                if (positionMessage['rd-x'] && positionMessage['rd-x'][0] != -1 && positionMessage['rd-y'][0] != -1) {
+                  data.latitude = utils.RD2lat(positionMessage['rd-x'][0], positionMessage['rd-y'][0]);
+                  data.longitude = utils.RD2lng(positionMessage['rd-x'][0], positionMessage['rd-y'][0]);
 
-                //   if (data.longitude < -180 || data.longitude > 180) {
-                //     delete data.longitude
-                //   }
+                  if (data.longitude < -180 || data.longitude > 180) {
+                    delete data.longitude
+                  }
 
-                //   if (data.latitude < -85 || data.latitude > 85) {
-                //     delete data.latitude
-                //   }
-                // }
+                  if (data.latitude < -85 || data.latitude > 85) {
+                    delete data.latitude
+                  }
+                }
 
-                // if (positionMessage.punctuality) {
-                //   data.delay_seconds = positionMessage.punctuality[0]
+                if (positionMessage.punctuality) {
+                  data.delay_seconds = positionMessage.punctuality[0]
           
-                //   if (positionMessage.punctuality[0] > 60) {
-                //     data.has_delay = true
-                //   } else {
-                //     data.has_delay = false
-                //   }                  
-                // }
+                  if (positionMessage.punctuality[0] > 60) {
+                    data.has_delay = true
+                  } else {
+                    data.has_delay = false
+                  }                  
+                }
 
-                // if(data.latitude && data.longitude) {
-                //   redisImportController.importData(data, pgPool)
-                // }
+                if(data.latitude && data.longitude) {
+                  redisImportController.importData(data, pgPool)
+                }
 
                 // redisImportController.updateData(id, data, pgPool);
               }
