68188d94226ab0469a3076ce482f5f80edf0bbca
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2020-05-22 16:28:50 +0200

    Datadog added to Ingestion

--- app/ingestion/server.js
+++ app/ingestion/server.js
@@ -1,3 +1,4 @@
+const tracer         = require('dd-trace').init();
 const express        = require('express');  
 const app            = express();   
 const Sentry         = require('@sentry/node');
