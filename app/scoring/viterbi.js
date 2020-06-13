7cf441b1bd4a0d6189dcff420184090c993c2f35
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2020-06-13 13:29:13 +0200

    Finishing proof of concept of Viterbi in JS

--- app/scoring/viterbi.js
+++ app/scoring/viterbi.js
@@ -1,6 +1,7 @@
 const { pool } = require('./controllers/db.js')
 const store = require('./redis-layer-store')
 const util = require('util')
+const _ = require('lodash')
 
 /**
  * Returns the probability of the most probable state sequence. 
@@ -11,48 +12,54 @@ const util = require('util')
  * @param {array} transProb Transition matrix with probability for each state to transition to a different state.
  * @param {object} emitProb Emission matrix with probability for each state to stay in this state.
  */
-
 function viterbi(obs, states, startProb, transProb, emitProb) {
-  const sequences = [[]]
 
-  // Set initial probabilities
-  states.map((state, index) => {
-    sequences[0][state] = { prob: startProb[state] * emitProb[0][state], prev: undefined }
+  const sequences = []
+  sequences[0] = {}
+
+  states.map(state => {
+    sequences[0][state] = { prob: startProb[state] * emitProb[state][obs[0]], prev: undefined }
   })
 
-  obs = obs.slice(1)
   obs.map((ob, index) => { // Loop over all measurements 
     if(index > 0) {
-      sequences[index] = []
-      states.map((state) => { // Loop over all vehicles
-        let maxTransProb = sequences[index - 1][states[0]]['prob'] * transProb[index - 1][states[0]][state] // Set initial max transition probability
-        let prevStateSelected = states[0]
-        
-        for(let i = 1; i < states.length; i++) { // Determine transition probability from vehicle to each vehicle
-          const stateTransProb = sequences[index - 1][states[i]]['prob'] * transProb[index - 1][states[i]][state] // startProb * 
-          if(transProb > maxTransProb) {
-            maxTransProb = stateTransProb
-            prevStateSelected = states[i]
-          }
-        }
+      sequences[index] = {}
 
-        const maxProb = maxTransProb * emitProb[index - 1][state][obs[index]]
-        console.log({ maxProb: maxProb, maxTransProb: maxTransProb, emitProb: emitProb })
+      states.map((state, stateIndex) => {
+        let maxTransProb = sequences[index - 1][states[0]]['prob'] * transProb[states[0]][state]
+        let prevStateSelected = states[0]
+
+        states.map((prevState, prevStateIndex) => {
+          if(prevStateIndex > 0) {
+            const stateTransProb = sequences[index - 1][prevState]['prob'] * transProb[prevState][state]
+            if(stateTransProb > maxTransProb) {
+              maxTransProb = stateTransProb
+              prevStateSelected = prevState
+            }
+          }
+        })
+
+        const maxProb = maxTransProb * emitProb[state][obs[index]]
         sequences[index][state] = { prob: maxProb, prev: prevStateSelected }
       })
-    }
+    }    
   })
 
-  let highestMatch = { prob: 0 }
+  const opt = []
+  let maxProb = 0
+  let previous = undefined
 
-
-  // highest_match = { "prob": 0 }
-  // for state in V[-1]:
-  //   if V[-1][state]["prob"] > highest_match["prob"]: 
-  //     highest_match = { "vehicle_id": state, "prob": V[-1][state]["prob"] }
+  for (let [key, value] of Object.entries(sequences[sequences.length - 1])) {
+    if(value.prob > maxProb) {
+      maxProb = value.prob
+      opt.push(key)
+      previous = key
+    }
+  }
 
   return sequences
 }
+
 
 /**
  * Returns vehicles within a given radius (meters) for a given timestamp. 
@@ -185,71 +192,60 @@ async function setMarkovLayer(lon, lat, timestamp) {
   return observations
 }
 
+function createObservationId(length = 15) { 
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return `obs_${text}`;
+}
+
 async function setMarkovModel(layers) {
-  const emitProb = []
+  const emitProb = {}
   const startProb = {}
   const transProb = []
   const states = []
-  
-  for(let i = 0; i < layers.length; i++) {
-    const layer = layers[i]
+  const obs = []
 
-    if(i > 0) {
-      emitProb.push({})
-      transProb.push({})
-    }
+  for(let i = 0; i < layers.length; i++) {
+    const observationId = createObservationId()
+    obs.push(observationId)
+    
+    layers[i].observationId = observationId
+
+    const layer = layers[i]
 
     for(let j = 0; j < layer.length; j++) {
       const vehicle = layer[j]
-
       if(!states.includes(vehicle.vehicle_id)) {
         states.push(vehicle.vehicle_id)
-      }
-
-      if(i === 0) {
         startProb[vehicle.vehicle_id] = vehicle.emissionProb
-      } else {
-        emitProb[i - 1][vehicle.vehicle_id] = vehicle.emissionProb
-        transProb[i - 1][vehicle.vehicle_id] = vehicle.transitionProb[vehicle.vehicle_id]
       }
     }
   }
 
-  // Fill missing vehicles in observations with zero
-  for(const key in states) {
-    const state = states[key]
-    for(let i = 0; i < layers.length - 1; i++) {
-      if(!(state in emitProb[i])) {
+  for(const index in states) {
+    const state = states[index]
+    emitProb[state] = {}
+    transProb[state] = {}
 
-        let prob = {}
-        for(let j = 0; j < states.length; j++) {
-          if(states[j] === state) {
-            prob[states[j]] = 1 
-          } else {
-            prob[states[j]] = 0
-          }
-        }
+    for(let i = 0; i < layers.length; i++) {
+      const layer = layers[i]
+      const vehicle = _.find(layer, { vehicle_id: state })
+      emitProb[state][layer.observationId] = vehicle ? vehicle.emissionProb : 0
 
-        emitProb[i][state] = prob
-      }
-
-      if(!(state in transProb[i])) {
-
-        let prob = {}
-        for(let j = 0; j < states.length; j++) {
-          if(states[j] === state) {
-            prob[states[j]] = 1 
-          } else {
-            prob[states[j]] = 0
-          }
-        }
-
-        transProb[i][state] = prob
+      for(let j = 0; j < layer.length; j++) {
+        const vehicle = layer[j]
+        const transitionProb = vehicle.transitionProb[vehicle.vehicle_id][state]
+        transProb[state][vehicle.vehicle_id] = transitionProb ? transitionProb : 0
       }
     }
   }
 
-  return { startProb: startProb, emitProb: emitProb, transProb: transProb, states: states }
+  return { obs: obs, startProb: startProb, emitProb: emitProb, transProb: transProb, states: states }
 }
 
 async function getMarkovLayers(userId) {
@@ -273,22 +269,22 @@ async function saveMarkovLayer(layer, userId) {
 }
 
 async function score(lon, lat, timestamp, userId) {
-
   const latestMarkovLayer = await setMarkovLayer(lon, lat, timestamp)
   const previousMarkovLayers = await getMarkovLayers(userId)
 
   const markovLayers = [...previousMarkovLayers ? previousMarkovLayers : [], latestMarkovLayer]
 
-  const { startProb, emitProb, transProb, states } = await setMarkovModel(markovLayers)
+  const { obs, startProb, emitProb, transProb, states } = await setMarkovModel(markovLayers)
 
   let result = false
   if(markovLayers.length > 1) {
-    result = await viterbi(markovLayers, states, startProb, transProb, emitProb)
+    // console.log({ markovLayers: markovLayers, states: states, startProb: startProb, transProb: transProb, emitProb: emitProb })
+    result = await viterbi(obs, states, startProb, transProb, emitProb)
   }
 
   await saveMarkovLayer(latestMarkovLayer, userId)
 
-  return result
+  return result.length > 0 ? result[result.length - 1] : result
   
 }
 
@@ -306,24 +302,23 @@ const data = [
     lon: 4.93552, 
     lat: 52.40296,
     timestamp: 1590954501
+  }, {
+    lon: 4.93591, 
+    lat: 52.40337,
+    timestamp: 1590954505
+  }, {
+    lon: 4.93604, 
+    lat: 52.40354,
+    timestamp: 1590954506
+  }, {
+    lon: 4.9361, 
+    lat: 52.40372,
+    timestamp: 1590954507
+  }, {
+    lon: 4.93599, 
+    lat: 52.40401,
+    timestamp: 1590954509
   }
-  // }, {
-  //   lon: 4.93591, 
-  //   lat: 52.40337,
-  //   timestamp: 1590954505
-  // }, {
-  //   lon: 4.93604, 
-  //   lat: 52.40354,
-  //   timestamp: 1590954506
-  // }, {
-  //   lon: 4.9361, 
-  //   lat: 52.40372,
-  //   timestamp: 1590954507
-  // }, {
-  //   lon: 4.93599, 
-  //   lat: 52.40401,
-  //   timestamp: 1590954509
-  // }
 ]
 
 ;(async () => {
@@ -334,7 +329,19 @@ const data = [
     for(let i = 0; i < data.length; i++) {
       const observation = data[i]
       const result = await score(observation.lon, observation.lat, observation.timestamp, userId)
-      // console.log(util.inspect({ result: result, userId: userId }, {showHidden: false, depth: null}))
+      
+      const opt = []
+      let maxProb = 0
+      let previous = undefined
+      for (let [key, value] of Object.entries(result)) {
+        if(value.prob > maxProb) {
+          maxProb = value.prob
+          opt.push(key)
+          previous = key
+        }
+      }
+      
+      console.log({ maxProb: maxProb, vehicleId: opt[opt.length - 1]})
       console.log('================================================================')
       outcome.push(result)
     }
@@ -347,24 +354,16 @@ const data = [
   console.log(err)
 })
 
-// const obs = [1, 2, 3, 4]
-// const states = ['Groningen', 'Den Haag', 'Enschede']
-// const startProb = { 'Groningen': 1, 'Den Haag': 2, 'Enschede': 3 }
-// const transProb = { 
-//  'Groningen':
-// }
-
-
-// const obs = ['normal', 'cold', 'dizzy']
-// const states = ['Healthy', 'Fever']
-// const startProb = { 'Healthy': 0.6, 'Fever': 0.4 }
-// const transProb = { 
-//   'Healthy': { 'Healthy': 0.7, 'Fever': 0.3 }, 
-//   'Fever': { 'Healthy': 0.4, 'Fever': 0.6 }
-// }
-// const emitProb = {
-//   'Healthy': { 'normal': 0.5, 'cold': 0.4, 'dizzy': 0.1 }, 
-//   'Fever': { 'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6 }
-// }
+const obs = ['normal', 'cold', 'dizzy']
+const states = ['Healthy', 'Fever']
+const startProb = { 'Healthy': 0.6, 'Fever': 0.4 }
+const transProb = { 
+  'Healthy': { 'Healthy': 0.7, 'Fever': 0.3 }, 
+  'Fever': { 'Healthy': 0.4, 'Fever': 0.6 }
+}
+const emitProb = {
+  'Healthy': { 'normal': 0.5, 'cold': 0.4, 'dizzy': 0.1 }, 
+  'Fever': { 'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6 }
+}
 
 // console.log(viterbi(obs, states, startProb, transProb, emitProb))
