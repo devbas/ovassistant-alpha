64ded5382cecbd581256678fe465176b11201d79
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2020-07-14 14:14:36 +0200

    Trajectory timing controller initiated

--- app/ingestion/controllers/trajectory-timing.js
+++ app/ingestion/controllers/trajectory-timing.js
@@ -9,19 +9,19 @@ const updateTrajectoryTiming = async ({ vehicleId, delaySeconds, measurementUnix
 
     const client = await pgPool.connect()
 
-    const { rows: trip } = await client.query(`SELECT trip_id FROM trips WHERE realtime_trip_id = ${vehicleId}`)
+    const { rows: trip } = await client.query(`SELECT trip_id FROM trips WHERE realtime_trip_id = $1`, [vehicleId])
 
     if(trip[0].trip_id) {
       await client.query(`UPDATE trip_times_partitioned TTP1
                             SET 
-                            TTP1.start_planned = TTP2.start_planned + TTP2.delay_seconds - ${delaySeconds}, 
-                            TTP1.end_planned = TTP2.end_planned + TTP2.delay_seconds - ${delaySeconds}, 
-                            TTP1.delay_seconds = ${delaySeconds}
+                            TTP1.start_planned = TTP2.start_planned + TTP2.delay_seconds - $1, 
+                            TTP1.end_planned = TTP2.end_planned + TTP2.delay_seconds - $2, 
+                            TTP1.delay_seconds = $3
                           FROM trip_times_partitioned TTP2
                           WHERE TTP1.triptime_id = TTP2.triptime_id
-                          AND TTP2.trip_id = ${trip[0].trip_id}
-                          AND TTP2.end_planned >= (${measurementUnix} + TTP2.delay_seconds - ${delaySeconds})
-                          AND TTP2.end_planned <= (${measurementUnix} + 1000) // Target for tuning`)
+                          AND TTP2.trip_id = $4
+                          AND TTP2.end_planned >= ($5 + TTP2.delay_seconds - $6)
+                          AND TTP2.end_planned <= ($7 + 1000) // Target for tuning`, [delaySeconds, delaySeconds, delaySeconds, trip[0].trip_id, measurementUnix, delaySeconds, measurementUnix])
     } else {
       console.log('no trip found for: ' + vehicleId)
     }
