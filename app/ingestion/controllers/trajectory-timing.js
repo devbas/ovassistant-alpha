39d04a85f1119890941d5608a02069ade819f7bf
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2020-07-14 14:19:44 +0200

    debugging

--- app/ingestion/controllers/trajectory-timing.js
+++ app/ingestion/controllers/trajectory-timing.js
@@ -12,16 +12,14 @@ const updateTrajectoryTiming = async ({ vehicleId, delaySeconds, measurementUnix
     const { rows: trip } = await client.query(`SELECT trip_id FROM trips WHERE realtime_trip_id = $1`, [vehicleId])
 
     if(trip[0].trip_id) {
-      await client.query(`UPDATE trip_times_partitioned AS TTP1
+      await client.query(`UPDATE trip_times_partitioned
                             SET 
-                            TTP1.start_planned = TTP2.start_planned + TTP2.delay_seconds - $1, 
-                            TTP1.end_planned = TTP2.end_planned + TTP2.delay_seconds - $2, 
-                            TTP1.delay_seconds = $3
-                          FROM trip_times_partitioned TTP2
-                          WHERE TTP1.triptime_id = TTP2.triptime_id
-                          AND TTP2.trip_id = $4
-                          AND TTP2.end_planned >= ($5 + TTP2.delay_seconds - $6)
-                          AND TTP2.end_planned <= ($7 + 1000)`, [delaySeconds, delaySeconds, delaySeconds, trip[0].trip_id, measurementUnix, delaySeconds, measurementUnix])
+                            start_planned = start_planned + delay_seconds - $1, 
+                            end_planned = end_planned + delay_seconds - $2, 
+                            delay_seconds = $3
+                          FROM trip_id = $4
+                          AND end_planned >= ($5 + delay_seconds - $6)
+                          AND end_planned <= ($7 + 1000)`, [delaySeconds, delaySeconds, delaySeconds, trip[0].trip_id, measurementUnix, delaySeconds, measurementUnix])
     } else {
       console.log('no trip found for: ' + vehicleId)
     }
