ff34ca92e02b3fe3d2f5223fbbdc0affd12ab661
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2020-07-14 14:20:11 +0200

    debugging

--- app/ingestion/controllers/trajectory-timing.js
+++ app/ingestion/controllers/trajectory-timing.js
@@ -17,7 +17,7 @@ const updateTrajectoryTiming = async ({ vehicleId, delaySeconds, measurementUnix
                             start_planned = start_planned + delay_seconds - $1, 
                             end_planned = end_planned + delay_seconds - $2, 
                             delay_seconds = $3
-                          FROM trip_id = $4
+                          WHERE trip_id = $4
                           AND end_planned >= ($5 + delay_seconds - $6)
                           AND end_planned <= ($7 + 1000)`, [delaySeconds, delaySeconds, delaySeconds, trip[0].trip_id, measurementUnix, delaySeconds, measurementUnix])
     } else {
